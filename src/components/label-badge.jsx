87726e7444e5843c09f6f51ac2f19bb8cdd5bf9c
Philip Schatz <phil@cnx.org>
2015-09-15 14:28:26 -0400

    fix dark colors for column labels

--- src/components/label-badge.jsx
+++ src/components/label-badge.jsx
@@ -1,20 +1,7 @@
 import * as BS from 'react-bootstrap';
 import React from 'react';
 
-import {KANBAN_LABEL} from '../helpers';
-
-// http://alienryderflex.com/hsp.html
-function isLight(hexColor) {
-  const color = {
-    r: parseInt(hexColor.substring(0, 2), 16),
-    g: parseInt(hexColor.substring(2, 4), 16),
-    b: parseInt(hexColor.substring(4, 6), 16)
-  };
-  return Math.sqrt(
-     color.r * color.r * .299 +
-     color.g * color.g * .587 +
-     color.b * color.b * .114) >= 130;
-}
+import {KANBAN_LABEL, isLight} from '../helpers';
 
 const LabelBadge = React.createClass({
   render() {
@@ -38,7 +25,7 @@ const LabelBadge = React.createClass({
         key={name}
         {...this.props}
         className={className}
-        style={{backgroundColor: label.color}}>
+        style={{backgroundColor: '#' + label.color}}>
         {name}
       </BS.Badge>
     );
