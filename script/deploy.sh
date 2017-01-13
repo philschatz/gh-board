6ab120ee4d18b04badd315a650718e56347e2ad0
Mathieu Dutour <mathieu@dutour.me>
2017-01-13 19:46:21 +0000

    commit dist in deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -10,8 +10,15 @@ else
   git branch -d gh-pages
   git checkout -b gh-pages
 
+  # remove .gitignore
+  rm .gitignore
+
   # build
   npm run build
+
+  # commmit the new dist folder
+  git add dist
+  git commit -m ':rocket: deploy new version'
 
   # force push to gh-pages
   git push -f origin gh-pages
