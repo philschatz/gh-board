03f551d59416ce4d07aaa806b425b4e59488d275
Mathieu Dutour <mathieu@dutour.me>
2017-01-14 20:20:51 +0000

    fix indent

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import CurrentUserStore from './user-store';
 import runtime from 'offline-plugin/runtime';
 
 runtime.install({
-	// When an update is ready, tell ServiceWorker to take control immediately:
-	onUpdateReady() {
-		console.log('update ready');
-		runtime.applyUpdate();
-	},
+  // When an update is ready, tell ServiceWorker to take control immediately:
+  onUpdateReady() {
+    console.log('update ready');
+    runtime.applyUpdate();
+  },
 
-	// Reload to get the new version:
-	onUpdated() {
-		console.log('updated');
-		location.reload();
-	}
+  // Reload to get the new version:
+  onUpdated() {
+    console.log('updated');
+    location.reload();
+  }
 });
 
 Recordo.initialize();
